82565e8a8e5d1796f759bea911edaf8547a68fe2
Petrenko <130425059+igorQA123@users.noreply.github.com>
2023-10-24 21:12:43 +0700

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,25 @@
+class Settings:
+    chrome_driver = "C:\skillfactory\chromedriver-win64\chromedriver.exe"
+    base_url = 'https://b2c.passport.rt.ru'
+    valid_email = 'igorpetrenko62@gmail.com'
+    valid_password = 'werty123'
+    valid_email_for_reg = 'igorpetrenko62@gmail.com'
+    invalid_email = 'wrrostelek@gmail.com'
+    invalid_password = 'dfgri321'
+    empty_email = ''
+    empty_password = ''
+    menu_of_type_auth = ['Телефон', 'Почта', 'Логин', 'Лицевой счёт']
+    placeholder_name_of_user = ['Мобильный телефон', 'Электронная почта', 'Логин', 'Лицевой счёт']
+    f_name = 'Игорь'
+    l_name = 'Петренко'
+    f_name_ = '-'
+    l_name_ = '-'
+    russian_generate_string = 'строка'
+    latin_generate_string = 'sstring'
+    chinese_chars = '我不会说汉怎么样你会说英语'
+    special_chars = '!@#${}$**%^&*(#)_+_+=//\[\}]]]'
+    numbers = 3875643
+    empty = ''
+    passw1 = 'Dgbvhrujjf54639jfgy'
+    passw2 = 'ed34kknf76hj989jjjk'
+    passw3 = 'qsfj76658hhjjk098jhn'
